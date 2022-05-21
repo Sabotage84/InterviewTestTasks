ed25d9416faea54c07b1ecd7459abce7f289be5e
Sabotage84 <sabotage84@mail.ru>
2022-05-22 00:39:48 +0300

    ADD simple triangle

---
+++ MindBox/MindboxTestClassLibrary/MindboxTestClassLibrary/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindboxTestClassLibrary
+{
+    public class Triangle
+    {
+        double a, b, c;
+
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double GetArea()
+        {
+            double p = (a + b + c) / 2;
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
---
+++ MindBox/MindboxTestClassLibrary/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MindboxTestClassLibrary;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodTriangleArea()
+        {
+            Triangle triangle = new Triangle(11.5, 12.5, 20);
+            Assert.AreEqual(66.249528, triangle.GetArea(), 0.000001);
+        }
+    }
+}
