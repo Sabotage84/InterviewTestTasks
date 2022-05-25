a047ed7dba8e91546ea079cae4a1d8ece2c39961
Sabotage84 <sabotage84@mail.ru>
2022-05-26 00:42:36 +0300

    ADD concrete handlers

---
+++ MindBox/MindboxTestClassLibrary/MindboxTestClassLibrary/CircleHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindboxTestClassLibrary
+{
+    class CircleHandler : ShapeHandler
+    {
+        double radius;
+        public CircleHandler(double r)
+        {
+            radius = r;
+        }
+
+        protected override IShape GetShape()
+        {
+            return new Circle(radius);
+        }
+    }
+}
---
+++ MindBox/MindboxTestClassLibrary/MindboxTestClassLibrary/TriangleHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindboxTestClassLibrary
+{
+    class TriangleHandler : ShapeHandler
+    {
+        double a_side, b_side, c_side; 
+        public TriangleHandler(double a, double b, double c)
+        {
+            a_side = a;
+            b_side = b;
+            c_side = c;
+        }
+
+        protected override IShape GetShape()
+        {
+            return new Triangle(a_side, b_side, c_side);
+        }
+    }
+}
