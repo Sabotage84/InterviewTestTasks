6428ed12d220b62ef2723c09974f28535879b1c6
Sabotage84 <sabotage84@mail.ru>
2022-06-08 16:50:08 +0300

    ADD transport class

---
+++ vehicle/vehicle/Transport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicle
+{
+    abstract class Transport
+    {
+        double averageFuelConsumption;
+        double tankCapacity;
+        double speed;
+
+        protected Transport(double averageFuelConsumption, double tankCapacity, double speed)
+        {
+            AverageFuelConsumption = averageFuelConsumption;
+            TankCapacity = tankCapacity;
+            Speed = speed;
+        }
+
+        public double AverageFuelConsumption { get => averageFuelConsumption; set => averageFuelConsumption = value; }
+        public double TankCapacity { get => tankCapacity; set => tankCapacity = value; }
+        public double Speed { get => speed; set => speed = value; }
+
+        public virtual double GetMaxDistance()
+        {
+            return 0;
+        }
+
+        public virtual double GetCurrentDistance()
+        {
+            return 0;
+        }
+
+    }
+}
