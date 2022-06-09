b90757cb90736cd53a789e14e1876db8b6e28212
Sabotage84 <sabotage84@mail.ru>
2022-06-10 00:36:13 +0300

    ADD car class

---
+++ vehicle/vehicle/PassengerCar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicle
+{
+    class PassengerCar : Transport
+    {
+        const int MAX_PASSENGERS = 4;
+
+        int currentPassengers;
+        public PassengerCar( double averageFuelConsumption, double tankCapacity, double speed, int curPas = 0) : base(averageFuelConsumption, tankCapacity, speed)
+        {
+            CurrentPassengers = curPas;
+        }
+
+        public int CurrentPassengers { get => currentPassengers; 
+        set
+            {
+                if (value < 0 || value > MAX_PASSENGERS)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    currentPassengers = value;
+                }
+            }
+        }
+
+
+    }
+}
