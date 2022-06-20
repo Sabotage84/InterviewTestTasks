b48706afc002eac94f2575daca6b53ae1299f870
Sabotage84 <sabotage84@mail.ru>
2022-06-20 22:23:50 +0300

    ADD truck class

---
+++ vehicle/vehicle/Truck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicle
+{
+    class Truck : Transport
+    {
+        public Truck(double averageFuelConsumption, double tankCapacity, double speed) : base(averageFuelConsumption, tankCapacity, speed)
+        {
+
+        }
+    }
+}
