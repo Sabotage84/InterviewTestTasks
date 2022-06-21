b4c8fb5dd874140985316ed950b8bdcd1b1fa989
Sabotage84 <sabotage84@mail.ru>
2022-06-21 21:46:49 +0300

    ADD sportcar class

---
+++ vehicle/vehicle/SportCarcs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicle
+{
+    class SportCarcs : Transport
+    {
+        public SportCarcs(double averageFuelConsumption, double tankCapacity, double speed) : base(averageFuelConsumption, tankCapacity, speed)
+        {
+        }
+    }
+}
