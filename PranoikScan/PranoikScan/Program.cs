e661a2a6ce0f2128cb120b8cf382ad14f0b85ab2
Sabotage84 <sabotage84@mail.ru>
2022-10-25 00:32:28 +0300

    ADD get paranoik scan v1

---
+++ PranoikScan/PranoikScan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PranoikScan
+{
+    class Program
+    {
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                IntPtr calcWindow = FindWindow(null, "EVE - Varl0n");
+
+                if (SetForegroundWindow(calcWindow))
+                    SendKeys.SendWait("v");
+
+                Thread.Sleep(2000);
+            }
+
+        }
+    }
+}
