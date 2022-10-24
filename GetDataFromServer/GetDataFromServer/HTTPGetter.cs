1d70efb943b7df36f4a1bda334e1fdce743d755a
Sabotage84 <sabotage84@mail.ru>
2022-10-24 22:19:43 +0300

    ADD get data from server start

---
+++ GetDataFromServer/GetDataFromServer/HTTPGetter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDataFromServer
+{
+    public class HTTPGetter
+    {
+        string addres;
+
+        public string Addres { get => addres; set => addres = value; }
+
+        public object GetData(string path)
+        {
+            object res=null;
+
+
+            return res;
+        }
+
+    }
+}
